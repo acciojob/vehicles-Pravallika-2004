a8b827b499aa01a92b75d339f0a7c47e547a6232
Your Name <you@example.com>
2024-10-05 10:19:16 +0530

    boat class added

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,24 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+
+public class boat implements WaterVehicle{
+    String name;
+    int capacity;
+    public boat(String name,int capacity)
+    {
+        this.name=name;
+        this.capacity=capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
+
